40328af1a4fe3d5ee9a0bf3aede3223f67450e0d
lzawbrito <75512521+lzawbrito@users.noreply.github.com>
2021-06-25 14:31:50 -0400

    Added music

--- src/components/music/MusicComponent.jsx
+++ src/components/music/MusicComponent.jsx
@@ -4,7 +4,7 @@ import NavigationComponent from "../NavigationComponent";
 export default class HomeComponent extends React.Component {
 	render() {
 		return<>
-		<p>this is music</p>
+		<p>Currently under construction.</p>
 		</>
 	}
 }
