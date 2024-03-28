09d65a98b710361a3444841823275d806f5794c3
Lucas Brito <luzabri7@gmail.com>
2024-03-27 22:03:04 -0700

    deploy

--- docs/notes/sync.sh
+++ docs/notes/sync.sh
@@ -1,18 +1,22 @@
 #!/bin/zsh
 
+# This will sync both the logs and the latex template and push to the site
+
+SITEDIR="/Users/lzawbrito/projects/lzawbrito.github.io/"
 SRC="/Users/lzawbrito/Documents/notes/"
-DEST="/Users/lzawbrito/projects/lzawbrito.github.io/public/docs/notes/"
+DEST="${SITEDIR}public/docs/notes/"
 
 rsync -av \
-	--exclude={'.*','*.mtc*','*.toc','*.maf','*.aux','_minted*','*.log'} \
+	--exclude={'.*','*.mtc*','*.toc','*.maf','*.aux','_minted*','*.log','scripts/'} \
 	${SRC} ${DEST}
 
 rsync -av \
 	{/Users/lzawbrito/latex-templates/lzawbrito-template.sty,/Users/lzawbrito/latex-templates/color-palette.tex} \
 	"${DEST}/latex-templates"
 
-# rsync /Users/lzawbrito/Documents/latex-templates/color-palette.tex \ 
-# 		${DEST}
-
+# deploy to site
+cd $SITEDIR
+./deploy.sh
+cd -
 
 	
